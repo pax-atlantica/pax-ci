afb7c06786c3b106d7f83a80e733be965ed4f7ef
dwrz <dwrz@dwrz.net>
2017-12-09 15:14:39 +0000

    Fix logic for request

--- pax-ci.js
+++ pax-ci.js
@@ -45,7 +45,7 @@ const execBuild = (cb) => {
 
 // ROUTING
 app.post('/admin/build', (req, res) => {
-  if (req.body.action === 'closed' || req.body.action === 'unlabeled' && req.body.merged === 'true') {
+  if (req.body.action === 'closed' && req.body.merged === 'true') {
     res.send('BUILDING');
     execBuild((err, success) => {
       if (err) {
