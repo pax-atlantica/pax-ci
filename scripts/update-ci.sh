24728764a16a2b8a0a957389eb9df4edf1818feb
dwrz <dwrz@dwrz.net>
2017-12-09 14:56:59 +0000

    Add scripts

---
+++ scripts/update-ci.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+cd ../
+git pull --rebase upstream master
+if  [ $? -eq 0 ]; then
+    npm install
+    if [ $? -eq 0 ]; then
+        npm run build
+        if [ $? -eq 0 ]; then
+            pm2 restart pax-ci
+            printf 'Update completed successfully.'
+            exit 0
+        else
+            printf 'Something went wrong with pm2.'
+            exit 4
+        fi
+    else
+        printf 'Something went wrong with npm run build.'
+        exit 3
+    fi
+    printf 'Something went wrong with npm install.'
+    exit 2
+else
+    printf 'Something went wrong with git pull.'
+    exit 1
+fi
+
