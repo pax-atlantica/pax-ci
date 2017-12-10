880bb8e4fad4f87424615329a49e59c2b18a0e18
dwrz <dwrz@dwrz.net>
2017-12-10 01:53:08 +0000

    Fix logic in update production script

--- scripts/update-production.sh
+++ scripts/update-production.sh
@@ -8,20 +8,22 @@ if  [ $? -eq 0 ]; then
         npm run build
         if [ $? -eq 0 ]; then
             pm2 restart server-index
-            printf 'Update completed successfully.'
-            exit 0
+            if [ $? -eq 0 ]; then
+                printf 'Update completed successfully.'
+                exit 0
+            else
+                printf 'Something went wrong with pm2.'
+                exit 4
+            fi
         else
-            printf 'Something went wrong with pm2.'
-            exit 4
+            printf 'Something went wrong with npm run build.'
+            exit 3
         fi
     else
-        printf 'Something went wrong with npm run build.'
-        exit 3
+        printf 'Something went wrong with npm install.'
+        exit 2
     fi
-    printf 'Something went wrong with npm install.'
-    exit 2
 else
     printf 'Something went wrong with git pull.'
     exit 1
 fi
-
